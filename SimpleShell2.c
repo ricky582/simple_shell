5da731940efeded08ff3270b91d1fdb046301dd2
Euan McAllister SE2020 <euan.mcallister.2020@uni.strath.ac.uk>
2022-01-27 13:09:53 +0000

    Upload New File

---
+++ SimpleShell2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int status = 0; 
+int main(void){
+do {
+printf("S>");
+status = read();
+}
+while (status == 0);
+return 0;
+}
+
+int read(){
+    char input[512];
+    int status = 0;
+    gets(input);
+if (strcmp(input, "exit") == 0){
+    status = 1; 
+}
+if (strcmp(input, "d") == 0){
+    status = 1; 
+}
+if (input == EOF){
+
+    status = 1;
+}
+return status;
+}

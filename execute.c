9a87a6ba444ae2851d15e7846cc21c06d67d0b82
Euan Mcallister <euan.mcallister.2020@uni.strath.ac.uk>
2022-02-17 10:22:09 +0000

    Removing all the prints

--- execute.c
+++ execute.c
@@ -5,15 +5,12 @@
 int execute(char * tokens[]){
 char * token =tokens[0] ;
 char *argv[511];
-printf("This is tokens[0] %s\n", tokens[0]);
 
-printf("This is the token %s\n", token);
+
 int j = 1;
 argv[0] = token;
 while (tokens[j] != NULL){
 argv[j] = tokens[j];
-printf("This is argv[%d]: %s and this is tokens[%d]: %s \n", j, argv[j], j, tokens[j]);
-i++;
 j++;
 }
 pid_t pid = fork(); 
@@ -22,13 +19,13 @@ if (pid < 0){
 
 }
 else if (pid  == 0){
-printf("I would now run execvp(%s,%s)\n", token, argv); 
+
 execvp(token, argv);
 memset(argv, NULL, sizeof(argv));
 }
 else{
 wait(NULL);
-printf("the child has completed");
+printf("the child has completed\n");
 
 }
 

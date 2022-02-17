6aa462adf082700f1f0ba0fbc6ea518d348b94d9
Ewan McCourt CS2020 <ewan.mccourt.2020@uni.strath.ac.uk>
2022-02-17 15:33:26 +0000

    Update Methods.c

--- Methods.c
+++ Methods.c
@@ -3,76 +3,67 @@
 #include <signal.h>
 #include <unistd.h>
 #include "header.h"
-#define max 512
 
 
-int parse(char input [512]){
-    char * tokens[512];
-    char * token = strtok(input, " \n\t|<>&;");
-    tokens[0] = token; 
-    int i = 1;
+
+
+
+
+
+void reader(String input){
+   if(fgets(input, 512, stdin) == NULL) {
+       
+        free(input);
+        exit(0);
+    }
+  
+
+}
+
+void parse(String input, String tokens[]){
+    String copy = malloc(sizeof(char)*strlen(input));
+    String token = strtok(input, " \n\t|<>&;");
+    strcpy(copy,input);
+    
+  
+    int i = 0; //Something is wrong to do with this future Ewan
      while(token != NULL ) {
       //printf( "%s\n", token ); //printing each token
+      tokens[i]=malloc(sizeof(char)*strlen(token));
+      strcpy(tokens[i],token);
       token = strtok(NULL, " \n\t|<>&;");
-      tokens[i] = token;
-      
       i++;
       
       }
-      execute(tokens);
-      return 0;
+ 
+    free(copy);
+    free(token);
+      
 }
 
 
-int flag =1;
-int reader(){
-    FILE *fp;
-    char input[512];
-    //input = malloc(512 * sizeof(char));
-    int status = 0;
-    flag = fgets(input,512, stdin);//Change to fgets and check if null then exit if true
-    if (flag != NULL){
-     parse(input);  
-    }else {
-      signal(SIGINT, flag);  
 
-    }
-                    //allocate memory for tokenised array
-                        //parse after if not null
-if (strcmp(input, "exit") == 0){
-     signal(SIGINT, flag);
-    status = 1; 
-}
-//free(input);
-//free memory
-return status;}
-
-
-
-
-int execute(char * tokens[]){
+int execute(String tokens[]){
 char * token =tokens[0] ;
-char *argv[511];
-
-
-int j = 1;
-argv[0] = token;
-while (tokens[j] != NULL){
-argv[j] = tokens[j];
-j++;
+ if(strcmp(tokens[0], "exit") == 0) {
+            return 1;
 }
+
+
 pid_t pid = fork(); 
+pid_t child;
 if (pid < 0){
     perror("Error!");
-
+   
 }
 else if (pid  == 0){
 
-execvp(token, argv);
-memset(argv, NULL, sizeof(argv));
+execvp(tokens[0], tokens);
+    
+    exit(0);
 }
 else{
-wait(NULL);
+child = wait(NULL);
 //printf("the child has completed\n");
 
 }

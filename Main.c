617660ed893aa812e40b2079c135be4b992018a2
robert <robert.sim.2020@uni.strath.ac.uk>
2022-02-23 22:12:00 +0000

    tidied up commands and fixed input overflow issue

--- Main.c
+++ Main.c
@@ -7,33 +7,26 @@
 
 int main(int argc, char *argv[]){
 
-//printf ("\nProgram Start.....\n\n");
-
-char *userInput;
-userInput = malloc(sizeof(char) * 512);
-
-
+    char *userInput;
+    userInput = malloc(sizeof(char) * 512);
+    //prompt for input
     printf("SSH>");
     while(fgets(userInput, 512, stdin)){
-        //printf("userInput:%s",userInput);
+            
+        //dumps input buffer if input is larger than character limit
+        if (*(userInput + strlen(userInput)-1) != '\n'){
+            for (int c; (c = getchar()) != EOF && c != '\n';);
+        }
 
-        if(strncmp(userInput, "exit", 4) ==0) {
-            //printf("breaking");
+        if(strncmp(userInput, "exit", 4) ==0) { //exit command
             return 0;
         }
-        parse(userInput);
-        
+
+        //input is parsed
+        parse(userInput); 
+
+        //next prompt for input
         printf("SSH>");
-        
     }
-    
     return 0;
-
-
 }
-
-
-
-
-
-
--- Methods.c
+++ Methods.c
@@ -7,57 +7,45 @@
 
 //Stage 1
 int parse(char input [512]){
+
     char * tokens[512];
     char * token = strtok(input, " \n\t|<>&;");
     tokens[0] = token; 
     int i = 1;
-     while(token != NULL ) {
-      token = strtok(NULL, " \n\t|<>&;");
-      tokens[i] = token;
-      
-      i++;
-      
-      }
-      execute(tokens);
-      return 0;
+    while(token != NULL ) { //input is tokenised
+        token = strtok(NULL, " \n\t|<>&;");
+        tokens[i] = token;
+        i++;
+    }
+    //tokenised input is executed
+    execute(tokens);
+    return 0;
 }
-
-
-
 
 //Stage 2
-
 int execute(char * tokens[]){
-char * token =tokens[0] ;
 
+    char * token =tokens[0] ;
+    //fork is created
+    pid_t pid = fork(); 
+    if (pid < 0){ //error occured
+        perror("Error!");
+    }
 
+    else if (pid  == 0){ //child process
+        //executes input as a command, if the input is not a command an error message will be displayed
+        if(execvp(tokens[0], tokens)==-1){
+            
+            char * errormsg = tokens[0];
+            perror(errormsg);
+            exit(0);
+        }
+    }
+    else{ //parent process
 
+        wait(NULL);
+    }
 
-pid_t pid = fork(); 
-if (pid < 0){
-    perror("Error!");
-
-}
-else if (pid  == 0){
-
-if(execvp(tokens[0], tokens)==-1);{
-
-    char * errormsg = tokens[0];
-    perror(errormsg);
-    exit(0);
-}
-}
-else{
-wait(NULL);
-
-}
-
-return 0;
+    return 0;
 
 }
-
-//Stage 3
-
-
-
-

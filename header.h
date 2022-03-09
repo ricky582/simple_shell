1529841080cfd6080aa0d0ee8e9e633134b9947d
Toby Weir CS2020 <toby.weir.2020@uni.strath.ac.uk>
2022-03-09 12:19:40 +0000

    Update header.h

--- header.h
+++ header.h
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+
+
 int parse(char input [512]);
 int reader();
 int execute(char * tokens[]);
+void setpath(char * tokens[]);
+void currentCWD();
+void cd(char * tokens[]);
+void getpath(char * tokens[]);

ef5b659ca4de1297a5ccc19722ded96dba3edb18
rowan <rowan.siebert.2020@uni.strath.ac.uk>
2022-03-10 11:23:30 +0000

    Stage 5 edits

--- header.h
+++ header.h
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+
+char commands[19][511];
+int count;
+void enterIntoArray(char input [512]);
 int parse(char input [512]);
 int reader();
 int execute(char * tokens[]);
